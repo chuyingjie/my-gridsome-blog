f568737d31817f265586a42c92c32d629053ab94
chu.yingjie <tochuyingjie@126.com>
2021-04-18 23:29:29 +0800

    layout

--- src/main.js
+++ src/main.js
@@ -2,9 +2,16 @@
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
 import DefaultLayout from '~/layouts/Default.vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import './assets/css/index.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import '@fortawesome/fontawesome-free/css/all.min.css'
+// import './assets/css/index.css'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
+import Vue from 'vue';
+import util from './utils/util'
+
+Vue.use(ElementUI)
+Vue.prototype.$util = util
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,159 +1,60 @@
 <template>
-  <div>
-      <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="index.html">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.html">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="post.html">Sample Post</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contact.html">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-  <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="site-heading">
-            <h1>Clean Blog</h1>
-            <span class="subheading">A Blog Theme by Start Bootstrap</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  </header>
-
-  <!-- Main Content -->
-  <div class="container">
-    <div class="row">
-      <div class="col-lg-8 col-md-10 mx-auto">
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Man must explore, and this is exploration at its greatest
-            </h2>
-            <h3 class="post-subtitle">
-              Problems look mighty small from 150 miles up
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 24, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
-            </h2>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on September 18, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Science has not yet mastered prophecy
-            </h2>
-            <h3 class="post-subtitle">
-              We predict too much for the next year and yet far too little for the next ten.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on August 24, 2019</p>
-        </div>
-        <hr>
-        <div class="post-preview">
-          <a href="post.html">
-            <h2 class="post-title">
-              Failure is not an option
-            </h2>
-            <h3 class="post-subtitle">
-              Many say exploration is part of our destiny, but it’s actually our duty to future generations.
-            </h3>
-          </a>
-          <p class="post-meta">Posted by
-            <a href="#">Start Bootstrap</a>
-            on July 8, 2019</p>
-        </div>
-        <hr>
-        <!-- Pager -->
-        <div class="clearfix">
-          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <hr>
-
-  <!-- Footer -->
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <ul class="list-inline text-center">
-            <li class="list-inline-item">
-              <a href="#">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-          </ul>
-          <p class="copyright text-muted">Copyright &copy; Your Website 2019</p>
-        </div>
-      </div>
-    </div>
-  </footer>
-  </div>
+  <Layout>
+    <h1>test</h1>
+  </Layout>
 </template>
 
 <script>
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
-  }
-}
+  name: "IndexPage",
+  data() {
+    return {
+      projects: [
+        {
+          login: "GitHub-Laziji",
+          id: 30425217,
+          node_id: "MDQ6VXNlcjMwNDI1MjE3",
+          avatar_url: "https://avatars.githubusercontent.com/u/30425217?v=4",
+          gravatar_id: "",
+          url: "https://api.github.com/users/GitHub-Laziji",
+          html_url: "https://github.com/GitHub-Laziji",
+          followers_url: "https://api.github.com/users/GitHub-Laziji/followers",
+          following_url:
+            "https://api.github.com/users/GitHub-Laziji/following{/other_user}",
+          gists_url:
+            "https://api.github.com/users/GitHub-Laziji/gists{/gist_id}",
+          starred_url:
+            "https://api.github.com/users/GitHub-Laziji/starred{/owner}{/repo}",
+          subscriptions_url:
+            "https://api.github.com/users/GitHub-Laziji/subscriptions",
+          organizations_url: "https://api.github.com/users/GitHub-Laziji/orgs",
+          repos_url: "https://api.github.com/users/GitHub-Laziji/repos",
+          events_url:
+            "https://api.github.com/users/GitHub-Laziji/events{/privacy}",
+          received_events_url:
+            "https://api.github.com/users/GitHub-Laziji/received_events",
+          type: "User",
+          site_admin: false,
+          name: "辣子鸡",
+          company: null,
+          blog: "laboo.top",
+          location: "fuzhou",
+          email: null,
+          hireable: null,
+          bio: "                              (°ー°〃) ",
+          twitter_username: null,
+          public_repos: 30,
+          public_gists: 17,
+          followers: 155,
+          following: 18,
+          created_at: "2017-07-25T02:47:47Z",
+          updated_at: "2021-03-31T02:08:48Z",
+        },
+      ],
+      loading: false,
+    };
+  },
+};
 </script>
 
 <style>
